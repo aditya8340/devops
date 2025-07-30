2f53dd75609a80051b010a49a4baa4f97b4070c2
aditya8340 <adikingyadav@gmail.com>
2025-07-30 17:35:53 +0000

    for pmd warning modified 3

--- PracA.java
+++ PracA.java
@@ -1,6 +1,9 @@
 public class PracA {
-    public void badMethod() {
-        int unused = 42; // PMD will flag this as "UnusedLocalVariable"
-    }
+	public void badMethod() {
+		int unused = 42; // PMD will flag this as "UnusedLocalVariable"
+	}
+	public static void main(Strinf []args){
+		
+	}
 }
 
